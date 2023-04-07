a400d7ce33194ad745d6637803494320d0273d20
Haritonov Vasilii <haritonov_vasya@inbox.ru>
2023-04-07 17:28:41 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,14 +7,13 @@ from scipy.stats import chi2
 
 chat_id = 291445198 # Ваш chat ID, не меняйте название переменной
 
+
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
-    loc = x.mean()
+    sum_pow = (x ** 2).sum()
     n = len(x)
-    df = n - 1
-    scale = np.sqrt(np.var(x))
-    
-    displ_coeff = 2.33
 
-    return np.sqrt((n-1) * scale*scale / chi2.ppf(1 - alpha/2, df) / 19 * displ_coeff), \
-           np.sqrt((n-1) * scale*scale / chi2.ppf(alpha/2, df) / 19 * displ_coeff)
+    left = np.sqrt(sum_pow / (19 * chi2.ppf(1 - alpha / 2, 2 * n)))
+    right = np.sqrt(sum_pow / (19 * chi2.ppf(alpha / 2, 2 * n)))
+    
+    return (left, right)
